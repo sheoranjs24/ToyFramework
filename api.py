886d5954e54bba539a5be8dbb11560c087104fea
Min Xiang <cff29546@gmail.com>
2015-02-03 13:27:31 -0800

    first commit

---
+++ api.py
@@ -0,0 +1,9 @@
+def get_value(key):
+  pass
+
+def set_value(key, value):
+  pass
+
+def del_key(key):
+  pass
+
---
+++ service.py
@@ -0,0 +1,11 @@
+def get_replica_set(key):
+  '''get a list of host id for all replicas of a key'''
+
+def get_host():
+  '''return host id of this node'''
+
+def send_message(host, msg):
+  '''send message'''
+
+def set_message_handler(handler):
+  '''set handler for all incomming message'''
