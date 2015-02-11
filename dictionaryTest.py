1d1fa8a3c9edc9cdd60ed0b2f0bf929f120def61
Prashant Chhabra <prashant.chhabra89@gmail.com>
2015-02-11 02:00:14 -0800

    Key Value Store

---
+++ dictionaryTest.py
@@ -0,0 +1,13 @@
+from dictionary import dictionary
+
+#just some methods to do testing
+print dictionary.get("iam")
+print dictionary.put("k1", "v1")
+print dictionary.get("zaza")
+print dictionary.get("k1")
+print dictionary.delete("k1")
+print dictionary.delete("k1")
+print dictionary.get("k1")
+
+print "last"
+print dictionary.put("test1","test2")
