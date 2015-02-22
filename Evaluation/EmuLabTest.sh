c01a0e3c1d8d63754be1e85f8391ebbb326c5a0a
Jyoti Sheoran <sheoranjs24@gmail.com>
2015-02-22 14:48:44 -0800

    bugfix bandwidth test

--- Evaluation/EmuLabTest.sh
+++ Evaluation/EmuLabTest.sh
@@ -9,7 +9,8 @@ sshFile='/Volumes/JSHome/Users/sheoranjs24/.ssh/emulab_rsa'
 nodeFile='EmulabNodes.txt'
 
 # Read node names from text file
-readarray nodes < $path$nodeFile
+#readarray nodes < $path$nodeFile
+IFS=$'\r\n' GLOBIGNORE='*' :; nodes=($(cat $path$nodeFile))
 
 # Explicitly report nodes
 let i=0
@@ -19,17 +20,15 @@ done
 
 # Test Bandwidth
 # connect to server
-host=$user+'@'+${nodes[0]}+$domain
-echo "host: $host"
-ssh -i $sshFile $host
-/usr/local/etc/emulab/emulab-iperf -s &
-exit
+server=$user'@'${nodes[0]}$domain
+echo "server: $server"
+ssh -i $sshFile $server 'bash -s' < $path'ServerScript.sh'
 # connect to client
-host=$user+'@'+${nodes[1]}+$domain
-echo "host: $host"
-ssh -i $sshFile $host
-/usr/local/etc/emulab/emulab-iperf -c server -f k -i 60 -t 60
-exit
+client=$user'@'${nodes[1]}$domain
+echo "client: $client"
+ssh -i $sshFile $client 'bash -s' <  $path'ClientScript.sh' ${nodes[0]}$domain >> /tmp/client.txt 
+cat /tmp/client.txt
+
 
 
 
