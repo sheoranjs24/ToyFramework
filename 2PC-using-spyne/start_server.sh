7201117f234ed0eec419f0f3825f88206fc1563c
Prashant Chhabra <prashant.chhabra89@gmail.com>
2015-02-25 16:20:47 -0800

    get public IP of server

--- 2PC-using-spyne/start_server.sh
+++ 2PC-using-spyne/start_server.sh
@@ -9,8 +9,11 @@ workon toyframework
 folder=`echo $HOST | cut -d. -f1`
 cd $folder
 
+# get public ip
+IPADDR=`ifconfig eth0 | grep "inet addr:" | cut -d: -f2 | awk '{ print $1}'`
+
 # start server
 DATE=$(date +"%Y%m%d%H%M")
-python $HOME/ToyFramework/2PC-using-spyne/server.py -H $HOST -P $SERVER_PORT > toyframework-$DATE.log 2>&1 &
+python $HOME/ToyFramework/2PC-using-spyne/server.py -H $IPADDR -P $SERVER_PORT > toyframework-$DATE.log 2>&1 &
 echo "server started!"
 
