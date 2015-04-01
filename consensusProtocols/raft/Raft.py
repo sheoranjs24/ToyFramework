1560f8da1c0bc1b0e5d7ffed286b21135e9e144a
Prashant Chhabra <prashant.chhabra89@gmail.com>
2015-04-01 13:54:36 -0700

    use ToyFramework logging feature

--- consensusProtocols/raft/Raft.py
+++ consensusProtocols/raft/Raft.py
@@ -66,14 +66,8 @@ class RaftServer(object):
       # send append entries
       # if success, commit & return to the client
       # tell commitIndex to all nodes to update
-  
-  def on_follower_timeout(self, message):
-    """ Leader timeout is reached. """
-  
-  def on_candidate_timeout(self, message):
-    """ Leader timeout is reached. """
        
-  def on_leader_timeout(self, message):
+  def on_timeout(self, message):
     """ Leader timeout is reached. """
   
   def _start_election(self, interface):
@@ -95,7 +89,7 @@ class RaftServer(object):
         
   def handle_vote_request(self, message):
     """ Vote request."""
-    if(self._last_vote is None and \
+    if (self._last_vote is None and \
       message['data']['lastLogIndex'] >= self._lastLogIndex):
       self._last_vote = message['sender']
       voteResponse = True
@@ -116,92 +110,76 @@ class RaftServer(object):
     """ Node received a vote."""
     if message['sender'] not in self._votes:
       self._votes[message['sender']] = message
-      if(len(self._votes.keys()) > interface.get_endpoints() / 2):
+      if (len(self._votes.keys()) > interface.get_endpoints() / 2):
         self._state = ReplicaState.Leader
         
-        # Start heartbeat
-        self._send_heart_beat(interface)
+        # Reset nodes indexes
         for ep in range(1, interface.get_endpoints()):
           node = str(interface.endpoints[ep])
           self._nextIndexes[node] = self._lastLogIndex + 1
           self._matchIndex[node] = 0
         
+        # Start heart-beat
+        self._send_heart_beat(interface)
+        
   def handle_append_entries(self, message, interface):
     """ Request to append an entry to the log. """
     self._timeoutTime = self._nextTimeout()
 
-    if(message['term'] < self._currentTerm):
+    if (message['term'] < self._currentTerm):
       self.send_message_response(message, interface, yes=False)
       return
 
-    if(message['data'] != {}):
-      log = self._log
+    if (message['data'] != {}):
+      log_index = interface.get_log_count() 
       data = message['data']
 
-      # Check if the leader is too far ahead in the log.
-      if(data['leaderCommit'] != self._commitIndex):
-        # If the leader is too far ahead then we
-        #   use the length of the log - 1
-        self._commitIndex = min(data['leaderCommit'],
-                                        len(log) - 1)
+      # Check if leaderCommit matches the replic's commitIndex
+      if (data['leaderCommit'] != self._commitIndex):
+        self._commitIndex = min(data['leaderCommit'], log_index - 1)
 
-      # Can't possibly be up-to-date with the log
-      # If the log is smaller than the preLogIndex
-      if(len(log) < data['prevLogIndex']):
+      # Check if log index is smaller than prevLogIndex
+      if (log_index < data['prevLogIndex']):
         self.send_message_response(message, interface, yes=False)
         return
 
-      # We need to hold the induction proof of the algorithm here.
-      #   So, we make sure that the prevLogIndex term is always
-      #   equal to the server.
-      if(len(log) > 0 and
-        log[data['prevLogIndex']]['term'] != data['prevLogTerm']):
-
-        # There is a conflict we need to resync so delete everything
-        #   from this prevLogIndex and forward and send a failure
-        #   to the server.
-        log = log[:data['prevLogIndex']]
+      # Make sure that the prevLogIndex term is equal to the Leader.
+      if (log_index > 0 and \
+          interface.get_log(log_index)[data['prevLogIndex']]['term'] != data['prevLogTerm']):
+        # Conflict: delete everything from this prevLogIndex 
+        interface.delete_log(index=data['prevLogIndex'])
         self.send_message_response(message, interface, yes=False)
-        self._log = log
         self._lastLogIndex = data['prevLogIndex']
         self._lastLogTerm = data['prevLogTerm']
         return 
-      # The induction proof held so lets check if the commitIndex
-      #   value is the same as the one on the leader
       else:
-        # Make sure that leaderCommit is > 0 and that the
-        #   data is different here
-        if(len(log) > 0 and
-         data['leaderCommit'] > 0 and
-         log[data['leaderCommit']]['term'] != message['term']):
+        # Check if the commitIndex value is equal to the leader.
+        if (log_index > 0 and \
+            data['leaderCommit'] > 0 and \
+            interface.get_log(log_index)[data['leaderCommit']]['term'] != message['term']):
           # Data was found to be different so we fix that
           #   by taking the current log and slicing it to the
           #   leaderCommit + 1 range then setting the last
           #   value to the commitValue
-          log = log[:self._commitIndex]
+          interface.delete_log(self._commitIndex)
           for e in data['entries']:
-            log.append(e)
+            interface.write_log(e)
             self._commitIndex += 1
 
           self.send_message_response(message, interface, yes=True)
-          self._lastLogIndex = len(log) - 1
-          self._lastLogTerm = log[-1]['term']
-          self._commitIndex = len(log) - 1
-          self._log = log
+          self._lastLogIndex =  interface.get_log_count() - 1
+          self._lastLogTerm = interface.get_log(self._lastLogIndex-1)['term']
+          self._commitIndex = interface.get_log_count() - 1
         else:
-          # The commit index is not out of the range of the log
-          #   so we can just append it to the log now.
-          #   commitIndex = len(log)
-          #   Is this a heartbeat?
+          # The commit index matches.
           if(len(data['entries']) > 0):
             for e in data['entries']:
-              log.append(e)
+              interface.write_log(e)
               self._commitIndex += 1
 
-            self._lastLogIndex = len(log) - 1
-            self._lastLogTerm = log[-1]['term']
-            self._commitIndex = len(log) - 1
-            self._log = log
+            self._lastLogIndex = interface.get_log_count() - 1
+            self._lastLogTerm = interface.get_log(self._lastLogIndex-1)['term']
+            self._commitIndex = interface.get_log_count() - 1
             self.send_message_response(message, interface, yes=True)
 
       self.send_message_response(message, interface, yes=True)
@@ -218,12 +196,12 @@ class RaftServer(object):
 
       # Get the next log entry to send to the client.
       previousIndex = max(0, self._nextIndexes[message['sender']] - 1)
-      previous = self._log[previousIndex]
-      current = self._log[self._nextIndexes[message['sender']]]
+      previous = interface.get_log(previousIndex)
+      current = interface.get_log(self._nextIndexes[message['sender']])
 
       # Send the new log to the client and wait for it to respond.
       appendEntry = {'timestamp': int(time.time()),
-                     'server': self._server,
+                     'sender': self._server,
                      'receiver': message['sender'],
                      'type': RaftMessage.AppendEntries,
                      'term': self._currentTerm,
@@ -248,7 +226,7 @@ class RaftServer(object):
   def _send_heart_beat(self, interface):
     """ Send AppendEntries message to replicas """
     message = {'timestamp': int(time.time()),
-               'server': self._server,
+               'sender': self._server,
                'receiver': None,
                'type': RaftMessage.AppendEntries,
                'term': self._currentTerm,
